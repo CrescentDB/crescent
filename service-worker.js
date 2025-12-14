214eb87229ad227af9f1560d68d5219c93b79a9c
bloodaary <aaryadevv8@gmail.com>
2025-12-14 20:44:10 +1100

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'crescent-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/main.css',
+  '/css/components.css',
+  '/css/features.css',
+  '/css/responsive.css',
+  '/js/app.js',
+  '/js/search.js',
+  '/js/ui.js',
+  '/assets/icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then((response) => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
